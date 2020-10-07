99cec0f9f07dabf100dad18cbfb09f98c91e2aa7
Magdalena Tomaszewska <szub_m@wp.pl>
2020-10-07 19:51:50 +0200

    TestStarter

--- src/main/java/org/TestStarter.java
+++ src/main/java/org/TestStarter.java
@@ -3,9 +3,9 @@ package org;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HelloWorld {
+public class TestStarter {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
         WebDriver driver;
 
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
